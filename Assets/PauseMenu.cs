dd7a0d7f623fb8827420074009355a7f5c1d6f4d
fatima17-09 <fatimahouran17@gmail.com>
2021-03-22 20:22:21 -0400

    added a pause and start menu with scripts

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+           
+            if (!GameIsPaused)
+            {
+               Pause();
+            }
+        }
+        /* else if (Input.GetKeyDown(KeyCode.P))
+        {
+            Pause();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            QuitGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+           LoadMenu();
+        } */
+
+
+
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // put it bag to normal game
+        GameIsPaused = false;
+        Debug.Log("ResumeClicked");
+    }
+     void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // freeze game
+        GameIsPaused = true;
+        Debug.Log("Paused");
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
